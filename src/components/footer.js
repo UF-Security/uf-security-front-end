55dda0f9bf580058a4a3d96d66bffec384aaef34
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2022-06-11 20:42:23 -0300

    Develop branch

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,8 @@ export default function Footer() {
       <div>Antonio Erisvan Alves Junior</div>
       <div>Eliaquim dos Santos Costa</div>
       <div>Maria Ester De Carvalho Veiga</div>
-      <div>Jâncy Wdson Coriolano de Aragão</div>              
+      <div>Jâncy Wdson Coriolano de Aragão</div>
+      <div className="pt-5">©2022</div>
     </footer>
   )
 }
